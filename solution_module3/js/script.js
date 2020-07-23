fbdfcf5dac633b66ab71c7935ffcbece78bc6072
unknown <niloydebwebmail@gmail.com>
2020-07-23 18:43:09 +0600

    My solution_module3

---
+++ solution_module3/js/script.js
@@ -0,0 +1,22 @@
+$(function () { // Same as document.addEventListener("DOMContentLoaded"...
+
+  $("#navbarToggle").blur(function (event) {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      $("#collapsable-nav").collapse('hide');
+    }
+  });
+});
+
+$(function () { 
+  $("#navbarToggle").blur(function (event) {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      $("#collapsable-nav").collapse('hide');
+    }
+  });
+//firefox and safari
+$("#navbarToggle").click(function (event) {
+    $(event.target).focus();
+  });
+});
